3946027d7af34e6f3205ec5f0a87d937ac8471ae
Kianna09 <kiannacroke@gmail.com>
2026-03-11 15:53:38 +0000

    Completed Delete Functions Lab

--- controllers/playlist.js
+++ controllers/playlist.js
@@ -18,15 +18,23 @@ const playlist = {
    },
 
    addSong(request, response) {
-    const playlistId = request.params.id;
-    const playlist = playlistStore.getPlaylist(playlistId);
-    const newSong = {
-      id: uuidv4(),
-      title: request.body.title,
-      artist: request.body.artist,
-    };
-    playlistStore.addSong(playlistId, newSong);
-    response.redirect('/playlist/' + playlistId);
+      const playlistId = request.params.id;
+      const playlist = playlistStore.getPlaylist(playlistId);
+      const newSong = {
+         id: uuidv4(),
+         title: request.body.title,
+         artist: request.body.artist,
+      };
+      playlistStore.addSong(playlistId, newSong);
+      response.redirect('/playlist/' + playlistId);
+   },
+
+   deleteSong(request, response) {
+      const playlistId = request.params.id;
+      const songId = request.params.songid;
+      logger.debug(`Deleting Song  $(songId} from Playlist ${playlistId}`);
+      playlistStore.removeSong(playlistId, songId);
+      response.redirect('/playlist/' + playlistId);
    },
 
 };
