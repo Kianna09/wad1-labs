13342249ab31d8912ced7cc4055f08e9ea52f55f
Kianna09 <kiannacroke@gmail.com>
2026-03-11 15:42:37 +0000

    Completed Add Functions Lab

--- models/playlist-store.js
+++ models/playlist-store.js
@@ -13,10 +13,17 @@ const playlistStore = {
     return this.store.findAll(this.collection);
   },
 
-   getPlaylist(id) {
-      return this.store.findOneBy(this.collection, (playlist => playlist.id === id));
-   },
+  getPlaylist(id) {
+    return this.store.findOneBy(this.collection, (playlist => playlist.id === id));
+  },
 
+  addSong(id, song) {
+    this.store.addItem(this.collection, id, this.array, song);
+  },
+
+  addPlaylist(playlist) {
+    this.store.addCollection(this.collection, playlist);
+  },
 };
 
 export default playlistStore;
